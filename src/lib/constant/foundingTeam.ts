8a777e3383d20017dc9b439615a4b872aad708b1
Md Aminul Islam <mr.aminul118@gmail.com>
2025-04-13 21:33:40 +0600

    first commit

---
+++ src/lib/constant/foundingTeam.ts
@@ -0,0 +1,32 @@
+const foundingTeam = [
+  {
+    photo: "https://i.ibb.co.com/pdvkBZp/fatema-cv-photo.png",
+    name: "Prof. Dr. Fatema Ashraf",
+    position: "Chairman, SHRL",
+    about_shrl:
+      "Smart Health Care and Research LTD (SHRL) is dedicated to advancing health research and clinical services with a focus on maternal, child, adolescent health. Committed to deliver highest standard of clinical care with empathy and an unwavering commitment, we have pioneered a path towards patient empowerment for effective management of gestational diabetes (GDM). A key part of our mission is to build research and innovation capacity to drive the development of a healthier Bangladesh. We are focused on identifying local challenges and crafting scientifically grounded, self- sustaining solutions to meet the unique health needs of our nation.",
+  },
+  {
+    photo: "https://i.ibb.co.com/DGmkgJF/Sharmin-phfbd.png",
+    name: "Prof. Dr. Sharmeen Yasmeen",
+    position: "Managing Director, SHRL",
+    about_shrl:
+      "SHRL strives to provide uncountable tender, loving care with a balance of heart, brain and expertise, supported by digital technologies, to ensure quality healthcare for all—regardless of financial means, social standing, or geographical location. We are also aiming to empower patients in diverse health situations. Additionally, our young professionals from different disciplines can use this platform for scientific and other innovations. Channeling the innovations through advocacy, marketing and related events are also included.",
+  },
+  {
+    photo: "https://i.ibb.co.com/YBQqrQH/moomtahina-2.png",
+    name: "Dr. Moomtahina Fatima",
+    position: "Director, SHRL",
+    about_shrl:
+      "SHRL is a dream project of a group of passionate professionals to ensure quality healthcare. We want to create a platform for all the stakeholders of the healthcare system in effective and meaningful ways utilizing evidence based research and digital technologies. Professionals will get the opportunity of certified research training and networking. Patients will have the scope of understanding their own health and ways to a healthy lifestyle. We welcome you all to the journey of empowerment.",
+  },
+  {
+    photo: "https://i.ibb.co.com/CMJ0SgZ/furkan.png",
+    name: "Furkan Hossain",
+    position: "Director of Research & Development, SHRL",
+    about_shrl:
+      "SHRL is founded to keep pace with the emerging demand of quality health care always and everywhere. To meet up the demand by large digital technology is adopted to transform health care in different meaningful way, increasing access to medical services, enhancing patient’s engagement, reducing costs, improving decision-making accuracy, accelerating information sharing, and advancing public health research. We are glad to invite new innovations, technologies, AI tools to empower the patient to ensure their health and well-being.",
+  },
+];
+
+export default foundingTeam;
---
+++ src/lib/constant/services.ts
@@ -0,0 +1,77 @@
+const services = [
+  {
+    photo: "https://i.ibb.co.com/Vv0Vtkh/doctor-patient.png",
+    service_name: "Online Consultation Platform",
+    description:
+      "BAN-GDM is Our flagship service, the online consultation platform provides secure and private access to healthcare professionals from the comfort of your home. This user-friendly platform ensures that women and families can receive online expert medical advice, especially for diabetes in pregnancy cases, consultations, and follow-up care without the need for physical visits, breaking down barriers to healthcare access.",
+    features_name: "Features",
+    features: [
+      "Secure, private consultations",
+      "Accessible anywhere in a fixed time",
+      "Focused on Maternal and Child Health",
+    ],
+  },
+  {
+    photo: "https://i.ibb.co.com/ykT4QYF/medical-research.png",
+    service_name: "Research and Analysis",
+    description:
+      "Our experienced research team is at the forefront of maternal and child health research. The expert team assists in conducting research in public health, clinical trials, analysis of secondary data and so on. We ensure that the latest evidence and the highest quality of research protocols back all of our research works. Our research efforts also contribute to the national healthcare community, fostering knowledge and innovation.",
+    features_name: "Focus Areas",
+    features: [
+      "Maternal and Child Health Research",
+      "Public Health Research",
+      "Clinical trials and health systems research",
+    ],
+  },
+  {
+    photo: "https://i.ibb.co.com/8rdKbcH/training-program.png",
+    service_name: "Training Programs for Healthcare Professionals",
+    description:
+      "We believe that well-trained healthcare professionals are the cornerstone of excellent patient care. Our specialized training programs focus on enhancing the skills and knowledge of healthcare providers, equipping them with the latest techniques and insights to offer exceptional care.",
+    features_name: "Training Includes",
+    features: [
+      "Workshops and seminars",
+      "Hands-on clinical training",
+      "Digital healthcare management skills",
+    ],
+  },
+  {
+    photo: "https://i.ibb.co.com/0tpTPCV/patient-education.png",
+    service_name: "Patient empowerment",
+    description:
+      "Empowering women and families with the knowledge and skills to engage them in the healthcare management steps, is in the core part of our mission. We provide a wide range of educational materials and resources to help individuals understand maternal and child health, nutrition, and preventive care, leading to healthier outcomes.",
+    features_name: "Resources Provided",
+    features: [
+      "Guideline on maternal and child healthcare and nutrition",
+      "Interactive educational tools, health tips and expert advices",
+      "Skills for self-management",
+    ],
+  },
+
+  {
+    photo: "https://i.ibb.co.com/mR3MrQm/community.png",
+    service_name: "Community Outreach",
+    description:
+      "Our commitment is to extend healthcare beyond individual consultations. We are actively engaged in outreach programs designed to bring quality healthcare services to undeserved and marginalized populations through digital solution. By collaborating with Government, INGOs, NGOs and Community organizations, we aim to ensure access and care to every woman and child",
+    features_name: "Outreach Programs",
+    features: [
+      "Access for undeserved and hard-to-reach populations",
+      "Health education workshops",
+      "Support for local health initiatives",
+    ],
+  },
+  {
+    photo: "https://i.ibb.co.com/L0w3xhT/research-80.png",
+    service_name: "Publications and Academic Collaboration",
+    description:
+      "As leaders in healthcare innovation, we regularly publish our research findings in esteemed academic journals. By collaborating with educational institutions and participating in global research initiatives, we contribute to the advancement of maternal and child healthcare on a global scale.",
+    features_name: "Academic Contributions",
+    features: [
+      "Publish research findings on maternal and child health",
+      "Collaboration with academic institutions",
+      "Participation in global healthcare conferences",
+    ],
+  },
+];
+
+export default services;

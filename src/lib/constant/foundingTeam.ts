e19a8b51dbe83a9f9ecf1dcaed04f242e4add69b
Md Aminul Islam <mr.aminul118@gmail.com>
2025-04-18 19:47:55 +0600

    Founding team image path change

--- src/lib/constant/foundingTeam.ts
+++ src/lib/constant/foundingTeam.ts
@@ -10,15 +10,15 @@ const foundingTeam: IFoundingTeam[] = [
       "Smart Health Care and Research LTD (SHRL) is dedicated to advancing health research and clinical services with a focus on maternal, child, adolescent health. Committed to deliver highest standard of clinical care with empathy and an unwavering commitment, we have pioneered a path towards patient empowerment for effective management of gestational diabetes (GDM). A key part of our mission is to build research and innovation capacity to drive the development of a healthier Bangladesh. We are focused on identifying local challenges and crafting scientifically grounded, self- sustaining solutions to meet the unique health needs of our nation.",
   },
   {
-    id:2,
-    photo: "/images/founding-team/Sharmin.png",
+    id: 2,
+    photo: "/images/founding-team/sharmin.png",
     name: "Prof. Dr. Sharmeen Yasmeen",
     position: "Managing Director, SHRL",
     about_shrl:
       "SHRL strives to provide uncountable tender, loving care with a balance of heart, brain and expertise, supported by digital technologies, to ensure quality healthcare for all—regardless of financial means, social standing, or geographical location. We are also aiming to empower patients in diverse health situations. Additionally, our young professionals from different disciplines can use this platform for scientific and other innovations. Channeling the innovations through advocacy, marketing and related events are also included.",
   },
   {
-    id:3,
+    id: 3,
     photo: "/images/founding-team/moomtahina.png",
     name: "Dr. Moomtahina Fatima",
     position: "Director, SHRL",
@@ -26,7 +26,7 @@ const foundingTeam: IFoundingTeam[] = [
       "SHRL is a dream project of a group of passionate professionals to ensure quality healthcare. We want to create a platform for all the stakeholders of the healthcare system in effective and meaningful ways utilizing evidence based research and digital technologies. Professionals will get the opportunity of certified research training and networking. Patients will have the scope of understanding their own health and ways to a healthy lifestyle. We welcome you all to the journey of empowerment.",
   },
   {
-    id:4,
+    id: 4,
     photo: "/images/founding-team/furkan.png",
     name: "Furkan Hossain",
     position: "Director of Research & Development, SHRL",

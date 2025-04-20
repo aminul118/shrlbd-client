948c6f2401b0c09523e477762e447e2858d5ce7f
Md Aminul Islam <mr.aminul118@gmail.com>
2025-04-21 01:26:06 +0600

    Google analytics allow

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,17 @@ import type { NextConfig } from "next";
 const securityHeaders = [
   {
     key: "Content-Security-Policy",
-    value:
-      "default-src 'self'; script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com; connect-src 'self' https://www.google-analytics.com; img-src 'self' https://www.google-analytics.com data:; object-src 'none'; frame-ancestors 'none';",
+    value: `
+      default-src 'self';
+      script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com;
+      style-src 'self' 'unsafe-inline';
+      connect-src 'self' https://www.google-analytics.com;
+      img-src 'self' https://www.google-analytics.com data:;
+      object-src 'none';
+      frame-ancestors 'none';
+    `
+      .replace(/\s{2,}/g, " ")
+      .trim(),
   },
   {
     key: "X-Frame-Options",

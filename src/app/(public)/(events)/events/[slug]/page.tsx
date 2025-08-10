e0409ebd184192d1f6afdcb2e79a40cae5a357f3
MD AMINUL ISLAM <mr.aminul118@gmail.com>
2025-08-10 13:32:17 +0600

    Origin ui navbar setup

--- src/app/(public)/(events)/events/[slug]/page.tsx
+++ src/app/(public)/(events)/events/[slug]/page.tsx
@@ -1,80 +1,10 @@
-import getEventsById from "@/lib/data/getEventsById";
-import { IParams, TEvents } from "@/lib/types/types";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import Image from "next/image";
-import Container from "@/components/ui/Container";
-import SectionHeading from "@/components/ui/SectionHeading";
-import { Metadata } from "next";
-import { generateMetaTags } from "@/lib/seo/genarateMetaTags";
 
-// ------> SEO Starts
-export async function generateMetadata({ params }: IParams): Promise<Metadata> {
-  const { slug } = await params;
-  const event: TEvents = await getEventsById(slug);
 
-  return generateMetaTags({
-    title: event.heading,
-    description: event.details?.slice(0, 160) || "Event at SHRLBD",
-    keywords: `shrl event, recent event of shrl, shrl event on ${event.time}`,
-    image: event.photo,
-    url: `https://www.shrlbd.com/events/${slug}`,
-  });
-}
-// ------> SEO TAG END
-
-const EventDetailsPage = async ({ params }: IParams) => {
-  const { slug } = await params;
-  const event: TEvents = await getEventsById(slug);
-
-  const { heading, date, time, venue, facilitators, photo, details } = event;
-
+const EventDetailsPage = () => {
   return (
-    <Container className="max-w-3xl">
-      <SectionHeading title="Event Details" />
-      <Card className="overflow-hidden shadow-md dark:bg-slate-900">
-        <div className="relative w-full h-[400px]">
-          <Image
-            src={photo}
-            alt={heading}
-            fill
-            className="object-cover"
-            priority
-          />
-        </div>
-
-        <CardHeader>
-          <CardTitle className="text-2xl">{heading}</CardTitle>
-        </CardHeader>
-
-        <CardContent className="space-y-2 text-gray-700 dark:text-gray-300">
-          {facilitators && (
-            <p>
-              <span className="font-semibold">Facilitator:</span> {facilitators}
-            </p>
-          )}
-          {date && (
-            <p>
-              <span className="font-semibold">Date:</span> {date}
-            </p>
-          )}
-          {time && (
-            <p>
-              <span className="font-semibold">Time:</span> {time}
-            </p>
-          )}
-          {venue && (
-            <p>
-              <span className="font-semibold">Venue:</span> {venue}
-            </p>
-          )}
-          {details && (
-            <p className="text-justify">
-              <span className="font-semibold">Details:</span> {details}
-            </p>
-          )}
-        </CardContent>
-      </Card>
-    </Container>
+    <div>
+      
+    </div>
   );
 };
 
---
+++ src/app/(public)/ai/page.tsx
@@ -0,0 +1,10 @@
+
+const AIPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AIPage;

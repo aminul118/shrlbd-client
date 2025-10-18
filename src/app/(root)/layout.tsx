80f16be2a1319bbe94ca4ac8584cf3c4d1e11435
MD AMINUL ISLAM <mr.aminul118@gmail.com>
2025-10-18 20:20:29 +0600

    Next js version opgrade to 15.5.6

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -4,17 +4,6 @@ import generateMetaTags from '@/seo/generateMetaTags';
 import { IChildren } from '@/types';
 import { Metadata } from 'next';
 
-// >> SEO Start
-export const metadata: Metadata = generateMetaTags({
-  title:
-    'Smart Healthcare and Research Ltd. - Empowering Maternal & Child Health',
-  description:
-    'Smart Healthcare and Research Ltd. offers innovative healthcare solutions for women and children, including online consultations, research, professional training, and community outreach.',
-  keywords:
-    "Smart Healthcare, SHRL, SHRLBD, Maternal health, Child healthcare, Digital healthcare services, Diabetes in pregnancy, Healthcare research, Public health, Professional training healthcare, Women's health, Child health, SHRL Bangladesh, Healthcare innovation",
-});
-// >> SEO End
-
 const MainLayout = ({ children }: IChildren) => {
   return (
     <div className="flex min-h-screen flex-col">
@@ -26,3 +15,14 @@ const MainLayout = ({ children }: IChildren) => {
 };
 
 export default MainLayout;
+
+// >> SEO Start
+export const metadata: Metadata = generateMetaTags({
+  title:
+    'Smart Healthcare and Research Ltd. - Empowering Maternal & Child Health',
+  description:
+    'Smart Healthcare and Research Ltd. offers innovative healthcare solutions for women and children, including online consultations, research, professional training, and community outreach.',
+  keywords:
+    "Smart Healthcare, SHRL, SHRLBD, Maternal health, Child healthcare, Digital healthcare services, Diabetes in pregnancy, Healthcare research, Public health, Professional training healthcare, Women's health, Child health, SHRL Bangladesh, Healthcare innovation",
+});
+// >> SEO End
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,18 +10,6 @@ import { GoogleAnalytics } from '@next/third-parties/google';
 import { Metadata } from 'next';
 import { Toaster } from 'sonner';
 
-// >> SEO Start
-export const metadata: Metadata = generateMetaTags({
-  title:
-    'Smart Healthcare and Research Ltd. - Empowering Maternal & Child Health',
-  description:
-    'Smart Healthcare and Research Ltd. offers innovative healthcare solutions for women and children, including online consultations, research, professional training, and community outreach.',
-  keywords:
-    "Smart Healthcare, SHRL, SHRLBD, Maternal health, Child healthcare, Digital healthcare services, Diabetes in pregnancy, Healthcare research, Public health, Professional training healthcare, Women's health, Child health, SHRL Bangladesh, Healthcare innovation",
-  image: '/seo/shrl-hero-ss.png',
-});
-// >> SEO End
-
 const RootLayout = ({ children }: IChildren) => {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -47,3 +35,15 @@ const RootLayout = ({ children }: IChildren) => {
 };
 
 export default RootLayout;
+
+// >> SEO Start
+export const metadata: Metadata = generateMetaTags({
+  title:
+    'Smart Healthcare and Research Ltd. - Empowering Maternal & Child Health',
+  description:
+    'Smart Healthcare and Research Ltd. offers innovative healthcare solutions for women and children, including online consultations, research, professional training, and community outreach.',
+  keywords:
+    "Smart Healthcare, SHRL, SHRLBD, Maternal health, Child healthcare, Digital healthcare services, Diabetes in pregnancy, Healthcare research, Public health, Professional training healthcare, Women's health, Child health, SHRL Bangladesh, Healthcare innovation",
+  image: '/seo/shrl-hero-ss.png',
+});
+// >> SEO End
